5ec407f828980140a4d0f8ca339e450d9697fd3e
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2025-05-18 19:39:29 -0300

    Update User.js
    lower case on username

--- backend/user_auth/model/User.js
+++ backend/user_auth/model/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }
 });
 
