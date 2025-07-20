7f04f72f82b92de75c791dc045220dc7c5b7dbe6
Gustavo Pereira <gustavo.pinzon55@gmail.com>
2025-07-20 17:09:24 -0300

    modify the vite.config.js to work with not ssl win vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./cert/key.pem'),
-      cert: fs.readFileSync('./cert/cert.pem'),
-    },
-    proxy: {
-      '/api': {
-        target: 'https://localhost:3000',
-        changeOrigin: true,
-        secure: false, // aceita certificados autoassinados no backend local
-      },
-    },
-    port: 5173, // opcional: escolha sua porta padrão
-    host: true, // opcional: permite acesso em rede local
-  },
-})
+  server: isDev
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, './cert/key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, './cert/cert.pem')),
+        },
+        proxy: {
+          '/api': {
+            target: 'https://localhost:3000',
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+        port: 5173,
+        host: true,
+      }
+    : {},
+});
