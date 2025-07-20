cce18c33dcb08ee1e83ed1f8ad469a8fb119a1ad
Gustavo Pereira <gustavo.pinzon55@gmail.com>
2025-07-20 18:55:28 -0300

    SSL problem again, RAVILONNNNNNNN

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,6 @@ const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
 
-const sslOptions = {
-  key: fs.readFileSync('./cert/key.pem'),
-  cert: fs.readFileSync('./cert/cert.pem'),
-};
-
 const app = express();
 
 const allowedOrigins = [
